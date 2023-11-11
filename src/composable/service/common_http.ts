d70accb1b86c92445c71f8baf084c33a3c4d3270
Elie Oko <elieoko100@gmail.com>
2023-11-11 12:21:20 +0100

    fix

--- src/composable/service/common_http.ts
+++ src/composable/service/common_http.ts
@@ -3,7 +3,7 @@ import axios, { type AxiosInstance } from "axios";
 //https://www.jstoolset.com/jwt
 export const useAxiosRequestWithToken = (tokens?:string) : AxiosInstance  =>{
     const useAxios: AxiosInstance = axios.create({
-        baseURL               : "http://213.136.74.84:50/api/",
+        baseURL               : "http://213.136.74.84:700/api/",
         headers   : {
           "Content-type"      :   "application/json",
           "X-Requested-With"  :   "XMLHttpRequest",
